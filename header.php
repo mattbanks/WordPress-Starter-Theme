a1692e76e06bdfb7a3e4570d2fda8a70eabfaee4
Matt Banks <mjbanks@gmail.com>
2012-11-20 12:04:25 -0500

    update to latest h5bp code, move style.css call to functions.php, cleanup

--- header.php
+++ header.php
@@ -1,29 +1,29 @@
-<!doctype html>
-<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <head>
 	<meta charset="utf-8">
 	<title><?php wp_title(''); ?></title>
 	<meta name="viewport" content="width=device-width">
 	<link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/favicon.ico">
 	<link rel="apple-touch-icon" href="<?php bloginfo('template_url'); ?>/apple-touch-icon.png">
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-	<!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
-		
+	<!--[if lt IE 7]>
+	    <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+	<![endif]-->
+
 	<div id="container">
 
 		<header>
-			<a id="logo" href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a>
+			<a id="logo" href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a>
 			<div class="description"><?php bloginfo('description'); ?></div>
 		</header>
 
 		<nav>
-			<?php wp_nav_menu( array('menu' => 'Main Menu' )); ?>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary' )); ?>
 		</nav>
