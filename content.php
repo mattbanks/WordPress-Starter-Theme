c0dcc1c47519224e9f8e25c92d464c3e98d6d777
Matt Banks <mjbanks@gmail.com>
2012-12-12 08:37:24 -0500

    add intro comment block

--- content.php
+++ content.php
@@ -1,3 +1,12 @@
+<?php
+/**
+ * The template used for displaying content
+ *
+ * @package mattbanks
+ * @since mattbanks 2.5
+ */
+?>
+
 <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 
 	<h1><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
