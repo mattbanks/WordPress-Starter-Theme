cd390aebe6870952418f19fa1cba20a89fd54133
Matt Banks <mjbanks@gmail.com>
2013-03-11 21:24:58 -0400

    updated compass settings

--- config.rb
+++ config.rb
@@ -3,22 +3,22 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "/"
-sass_dir = "scss"
-images_dir = "img"
-javascripts_dir = "js"
+sass_dir = "assets/scss"
+images_dir = "assets/images"
+javascripts_dir = "assets/js"
+fonts_dir = "assets/fonts"
 
-# You can select your preferred output style here (can be overridden via the command line):
 output_style = :compressed
 
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
+color_output = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
 # and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+# sass-convert -R --from scss --to sass assets/scss scss && rm -rf sass && mv scss sass
+preferred_syntax = :scss
