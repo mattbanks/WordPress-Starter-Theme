75026374b85f6fab0c285da42d6a3e601d7ff5c2
Yuval Maman <yuvilio@yahoo.com>
2013-04-25 11:18:07 -0400

    Separate the generated part of style.css to it's own file in assets/css/ so that we don't have to store it in the repository

--- config.rb
+++ config.rb
@@ -2,7 +2,7 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "/"
+css_dir = "assets/css"
 sass_dir = "assets/scss"
 images_dir = "assets/images"
 javascripts_dir = "assets/js"
