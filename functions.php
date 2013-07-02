422b260b2d5252ddf4262ca9df10162a3e6ec2e1
Matt Banks <mjbanks@gmail.com>
2013-07-02 08:59:53 -0400

    update yoast seo metabox filtering to not use an anonymous function since it doesn't work in php 5.2

--- functions.php
+++ functions.php
@@ -30,3 +30,11 @@ Misc Theme Functions
 function mb_add_post_type_caps() {
 	// mb_add_capabilities( 'portfolio' );
 }
+
+/**
+ * Filter Yoast SEO Metabox Priority
+ */
+add_filter( 'wpseo_metabox_prio', 'mb_filter_yoast_seo_metabox' );
+function mb_filter_yoast_seo_metabox() {
+	return 'low';
+}
