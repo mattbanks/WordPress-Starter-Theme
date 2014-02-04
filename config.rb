3346f6e8ff081b582580b454000942b97eb07027
Matt Banks <mjbanks@gmail.com>
2014-02-04 13:05:24 -0500

    move scss directory in config

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "/"
-sass_dir = "assets/scss"
+sass_dir = "assets/styles/source"
 images_dir = "assets/images"
 javascripts_dir = "assets/js"
 fonts_dir = "assets/fonts"
