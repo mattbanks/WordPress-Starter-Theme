6983460f25f978fa30be52e21575643991f927a8
Matt Banks <mjbanks@gmail.com>
2014-03-27 13:50:16 -0400

    remove footer sidebar by default since it's not being registered right away

--- footer.php
+++ footer.php
@@ -1,9 +1,8 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<?php dynamic_sidebar( 'Footer' ); ?>
 		<div class="site-info">
-			<p>&copy; <?php echo date( "Y" ); echo " "; bloginfo( 'name' ); ?></p>
+			<p class="copyright">&copy; <?php echo date( "Y" ); echo " "; bloginfo( 'name' ); ?></p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 
