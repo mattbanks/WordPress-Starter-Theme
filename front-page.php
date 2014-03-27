b9d7237a3a515bbdc966707910598061c79491b7
Matt Banks <mjbanks@gmail.com>
2014-03-27 12:36:47 -0400

    update front page template

--- front-page.php
+++ front-page.php
@@ -1,27 +1,32 @@
-<?php get_header(); ?>
+<?php
+/**
+ * The template for displaying the front page.
+ *
+ * This is the template that displays on the front page only.
+ *
+ * @package _mbbasetheme
+ */
 
-	<section id="main" role="main">
+get_header(); ?>
 
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
 
-		<article class="post" id="post-<?php the_ID(); ?>">
+			<?php while ( have_posts() ) : the_post(); ?>
 
-			<h1><?php the_title(); ?></h1>
+				<?php get_template_part( 'content', 'page' ); ?>
 
-			<div class="entry">
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template
+					if ( comments_open() || '0' != get_comments_number() ) :
+						comments_template();
+					endif;
+				?>
 
-				<?php the_content(); ?>
+			<?php endwhile; // end of the loop. ?>
 
-				<?php wp_link_pages( array( 'before' => 'Pages: ', 'next_or_number' => 'number' ) ); ?>
-
-			</div>
-
-		</article>
-
-		<?php endwhile; endif; ?>
-
-	</section> <!-- /#main -->
+		</main><!-- #main -->
+	</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
-
 <?php get_footer(); ?>
