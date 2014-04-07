26024582ef15e33513b3677604e4e2bd104e480e
Matt Banks <mjbanks@gmail.com>
2014-04-07 11:01:25 -0400

    add additional content for 404 template by default

--- 404.php
+++ 404.php
@@ -20,6 +20,33 @@ get_header(); ?>
 
 					<?php get_search_form(); ?>
 
+					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+
+					<?php if ( _mbbasetheme_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
+					<div class="widget widget_categories">
+						<h2 class="widgettitle"><?php _e( 'Most Used Categories', '_mbbasetheme' ); ?></h2>
+						<ul>
+						<?php
+							wp_list_categories( array(
+								'orderby'    => 'count',
+								'order'      => 'DESC',
+								'show_count' => 1,
+								'title_li'   => '',
+								'number'     => 10,
+							) );
+						?>
+						</ul>
+					</div><!-- .widget -->
+					<?php endif; ?>
+
+					<?php
+					/* translators: %1$s: smiley */
+					$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', '_mbbasetheme' ), convert_smilies( ':)' ) ) . '</p>';
+					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+					?>
+
+					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
+
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
